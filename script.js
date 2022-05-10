64d02fcead2ccaf8769d1c6ca987dacd37a48287
Ryan Safarzadeh <safarzadehryan@gmail.com>
2022-05-10 10:25:01 -0700

    Finish DOM integration, add winner announcement

---
+++ script.js
@@ -0,0 +1,101 @@
+
+let computerScore = 0;
+let playerScore = 0;
+
+const results = document.querySelector('.results');
+const score = document.querySelector('.score');
+const container = document.querySelector('.container');
+const scorePlayer = document.querySelector('.player-score');
+const scoreComputer = document.querySelector('.computer-score');
+const resetButton = document.querySelector('.reset-button');
+
+resetButton.addEventListener('click', () => {
+  playerScore = 0;
+  computerScore = 0;
+  scorePlayer.textContent = playerScore;
+  scoreComputer.textContent = computerScore;
+  results.textContent = "Let's go!";
+})
+
+
+
+const playerInputRock = document.querySelector('.rock');
+playerInputRock.addEventListener('click', () => {
+    results.textContent = playRound('rock');
+    scorePlayer.textContent = playerScore;
+    scoreComputer.textContent = computerScore;
+});
+
+const playerInputPaper = document.querySelector('.paper');
+playerInputPaper.addEventListener('click', () => {
+    results.textContent = playRound('paper');
+    scorePlayer.textContent = playerScore;
+    scoreComputer.textContent = computerScore;
+});
+
+const playerInputScissors = document.querySelector('.scissors');
+playerInputScissors.addEventListener('click', () => {
+    results.textContent = playRound('scissors');
+scorePlayer.textContent = playerScore;
+scoreComputer.textContent = computerScore;
+  
+});
+
+
+
+
+function createParagraph(text) {
+const p = document.createElement('p');
+p.textContent = text;
+return p;
+}
+
+function computerPlay() {
+    let handChoice = ['rock', 'paper', 'scissors'];
+    let chance = Math.floor((Math.random() * 3) + 1);
+    if (chance == 1) {
+        return handChoice[0];
+    } else if (chance == 2) {
+        return handChoice[1];
+    } else if (chance == 3) {
+        return handChoice[2];
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    computerSelection = computerPlay();
+
+    if (makeAllLowerCase(playerSelection) == computerSelection) {
+        return `Tie! You both chose ${makeAllLowerCase(computerSelection)}!` 
+    } else if 
+((makeAllLowerCase(playerSelection) == 'paper' && computerSelection == 'rock') ||
+(makeAllLowerCase(playerSelection) == 'scissors' && computerSelection == 'paper') ||
+(makeAllLowerCase(playerSelection) == 'rock' && computerSelection == 'scissors')) {
+        playerScore++;
+  return `Congrats, you win! ${makeFirstUpperCase(makeAllLowerCase(playerSelection))} beats ${computerSelection}!`;
+        
+    } else {
+        computerScore++;
+      return `Sorry, you lose! ${makeFirstUpperCase(computerSelection)} beats ${makeAllLowerCase(playerSelection)}!`;        
+    }
+
+    function makeFirstUpperCase(text) {
+       return text = text.charAt(0).toUpperCase() + text.slice(1);
+    }
+
+    function makeAllLowerCase(text) {
+        return text = text.toLowerCase();
+    }
+  
+scorePlayer.textContent = playerScore;
+scoreComputer.textContent = computerScore;
+}
+
+if (computerScore >= 5 || playerScore >= 5) {
+  results.textContent = 'Game Over!';
+}
+
+
+
+
+
